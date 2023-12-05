3b42da902baa0e808b81a47a3b5e3567e6eaf4a6
Andrey Okhotin <44405666+andrey-okhotin@users.noreply.github.com>
2023-12-05 17:36:26 +0300

    MainCode

--- lib/saving_utils/get_repo_root.py
+++ lib/saving_utils/get_repo_root.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 def get_repo_root():
     path_var = Path(os.getcwd())
-    while path_var.name != 'SS-DDPM':
+    while path_var.name != 'star-shaped':
         path_var = path_var.parent
     return path_var
